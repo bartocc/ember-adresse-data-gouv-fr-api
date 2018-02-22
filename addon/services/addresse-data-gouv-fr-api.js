a4df8005c67048fd8649413d46bd2324f2185ae3
Julien Palmas <julien.palmas@gmail.com>
2018-02-22 16:44:23 +0100

    implement search api

--- addon/services/addresse-data-gouv-fr-api.js
+++ addon/services/addresse-data-gouv-fr-api.js
@@ -7,6 +7,13 @@ export default Service.extend({
   ajax: inject(),
   origin: 'https://api-adresse.data.gouv.fr',
 
+  search(q) {
+    const pathname = '/search';
+    const endpoint = `${this.get('origin')}${pathname}`;
+
+    return this.get('ajax').request(endpoint, {data: {q}});
+  },
+
   // location : {lat: ..., lon: ...}
   reverse(location) {
     let reverseUrl = `${get(this, 'origin')}/reverse/?lon=${location.longitude}&lat=${location.latitude}`;
